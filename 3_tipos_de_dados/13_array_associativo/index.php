9dd3caa2e36616cd6eea7c9418434353e11735e7
FabricioSouzaa <souza.fabricio8@gmail.com>
2025-05-30 14:59:57 -0300

    layout ajuste

--- 3_tipos_de_dados/13_array_associativo/index.php
+++ 3_tipos_de_dados/13_array_associativo/index.php
@@ -1,15 +1,36 @@
 <?php
 
-  $arr = ['name' => 'Fabrício', 'age' => 24, 'favcolor' => 'black'];
+  $arr = [
+    'name' => 'Fabrício', 
+    'age' => 24, 
+    'favcolor' => 'black'
+  ];
 
   echo $arr['name'];
   echo "<br>";
   print_r($arr);
   echo "<br>";
   echo $arr['favcolor'];
+  echo "<br>";
   // Associative array
 
-  $arrAssoc = ['chave' => 'valor', 'bool' => true];
+  $arrAssoc = [
+    'chave' => 'valor',
+    'bool' => true
+  ];
 
-  echo "<br>";
   print_r($arrAssoc);
+  echo "<br>";
+
+  $arrCar = [
+    'marca' => 'Porsche',
+    'modelo' => 'GT3',
+    'ano' => 2024,
+    'cor' => 'Azul'
+  ];
+
+  print_r($arrCar);
+  echo "<br>";
+  echo $arrCar['modelo'];
+  echo "<br>";
+  echo $arrCar['cor'];
