46493511467ef981f95be599d223de0b7765af0f
Fabricio Souza <fabriciosouza@MacBook-Air-de-Fabricio.local>
2025-05-30 11:03:41 -0300

    Primeiro commit

---
+++ 3_tipos_de_dados/12_arrays/index.php
@@ -0,0 +1,21 @@
+<?php
+
+  $a = [1, 2, 3];
+
+  print_r($a);
+  echo "<br>";
+  echo $a[2];
+
+  $arr = ["Fabrício Souza", 2001, true];
+
+  echo "<br>";
+  print_r($arr);
+  echo "<br>";
+  print_r($arr[0]);
+
+  $b = ["Fabrício EDM", 4142, true];
+
+  echo "<br>";
+  print_r($b);
+  echo "<br>";
+  print_r($b[1]);
