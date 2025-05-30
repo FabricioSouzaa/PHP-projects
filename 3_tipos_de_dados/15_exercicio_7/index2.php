c3ff88095f6153ee75245df7de80ccbbc22222cf
FabricioSouzaa <souza.fabricio8@gmail.com>
2025-05-30 14:57:06 -0300

    adicionando arquivo index2.php do desafio

---
+++ 3_tipos_de_dados/15_exercicio_7/index2.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = [
+    'name' => 'Fabrício',
+    'age' => 16,
+    'city' => 'Santa Catarina'
+]
+
+if ($arr['age'] >= 18) {
+    echo "Você é maior de idade.";
+} else {
+    echo "Você é menor de idade.";
+}
