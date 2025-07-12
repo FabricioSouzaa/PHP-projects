67e47a4c5c0deff6ab2d6f048aa427676f5f6d05
FabricioSouzaa <souza.fabricio8@gmail.com>
2025-07-12 12:47:43 -0300

    exercises while

--- 7_estruturas_de_repeticao/1_while/index.php
+++ 7_estruturas_de_repeticao/1_while/index.php
@@ -1,65 +1,26 @@
 <?php
 
-  // A definição do contador
-  $x = 0;
+$x = 1;
 
-  // Início / Definição da estrutura
-  while($x < 10) {
+while ($x < 15) {
+  echo $x . "<br>";
+  $x += 1;  // ou $x = $x + 1; -> ou $x++
+}
 
-    // Corpo do loop
-    echo $x . "<br>";
+echo "<br>";
 
-    // Incremento do contador
-    $x++;
+$y = 10;
 
-  }
+while ($y <= 100) {
+  echo $y . "<br>";
+  $y += 10;
+}
 
-  echo "Continuando código <br>";
+echo "<br>";
 
-  // A definição do contador
-  $y = 0;
+$z = 10;
 
-  // Início / Definição da estrutura
-  while($y <= 10) {
-
-    // Corpo do loop
-    echo $y . "<br>";
-
-    // Incremento do contador
-    $y += 2;
-
-  }
-
-  echo "Continuando código <br>";
-
-  // A definição do contador
-  $z = 10;
-
-  // Início / Definição da estrutura
-  while($z > 0) {
-
-    // Corpo do loop
-    echo $z . "<br>";
-
-    // Incremento do contador
-    $z--;
-
-  }
-
-  echo "Continuando código <br>";
-
-  // A definição do contador
-  $a = 10;
-
-  // Início / Definição da estrutura
-  while($a > 0) {
-
-    // Corpo do loop
-    if($a % 2 != 0) {
-      echo $a . "<br>";
-    }
-
-    // Incremento do contador
-    $a--;
-
-  }
+while ($z > 0) {
+  echo $z . "<br>";
+  $z -= 1;  // ou $z = $z - 1; -> ou $z--
+}
--- 7_estruturas_de_repeticao/2_exercicio_27/index.php
+++ 7_estruturas_de_repeticao/2_exercicio_27/index.php
@@ -1,16 +1,13 @@
 <?php
 
-  $arr = [5, "Matheus", true, false, "Opa", 12.8, "Teste", true, [], "Palavra", 5, 10, "Alô"];
+$arr = [true, 10, "vermelho", 10.5, "azul", 20, "verde", 30, "amarelo", 40];
 
-  $x = count($arr);
-  $y = 0;
+$x = count($arr);
+$y = 0;
 
-  while($y < $x) {
-
-    if(is_string($arr[$y])) {
-      echo $arr[$y] . "<br>";
-    }
-
-    $y++;
-
+while ($y <= $x) {
+  if (is_string($arr[$y])) {
+    echo $arr[$y] . "<br>";
   }
+  $y++;
+}
